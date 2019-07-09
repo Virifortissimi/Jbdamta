3491c7410734c8523ce93136bce16ce821b706ed
David.O <ogunejimitedavid@gmail.com>
2019-07-09 13:07:58 +0100

    changed the url processing the form request....

--- contact.php
+++ contact.php
@@ -92,35 +92,37 @@
           </div>
           <!-- <div class="col-lg-1 col-md-1 col-sm-12"></div> -->
           <div class="col-lg-8 col-md-8 col-sm-12 form-bg">
-            
-            <form id="contact" action="<?= $_SERVER['PHP_SELF']; ?>" method="post">
+
+            <form id="contact" action="/Jbdamta/form_process.php" method="post">
                <h3>Contact Us</h3> 
                 <h4>Contact us today and get a reply in 24hours</h4>
                 <div class="row">
-                 
-                 
+
+
                   <fieldset>
-                    <input type="text" tabindex="1" placeholder="Your Name" value="<?= $name ?>" s autofocus>
-                    <span class="error"><?= $name_error ?></span>
+                      <label for="name">Name:</label>
+                      <input id="name" tabindex="1" type="text" placeholder="Your Name"  name="name" autofocus/>
+                      <span class="error"><?= $name_error ?></span>
                   </fieldset>
 
                   <fieldset>
-                    <input type="text" tabindex="2" placeholder="Your Email Address" value="<?= $email ?>" name="email" >
+                      <label for="email">Email Address:</label>
+                    <input type="text" tabindex="2" id="email" placeholder="Your Email Address" value="<?= $email ?>" name="email" >
                     <span class="error"><?= $email_error ?></span>
                   </fieldset>
 
                   <fieldset>
-                    <input type="text" tabindex="3" placeholder="Your Phone Number" value="<?= $phone ?>" name="phone">
-                    <span class="error"><?= $phone_error ?></span>
+                      <label for="phone">Phone:</label>
+                      <input type="text" id="phone" tabindex="3" placeholder="Your Phone Number" value="<?= $phone ?>" name="phone">
+                      <span class="error"><?= $phone_error ?></span>
                   </fieldset>
 
                   <fieldset>
-                    <textarea tabindex="4" placeholder="Type your message here..." value="<?= $message ?> name="message"></textarea>
+                      <label for="message">Message:</label>
+                    <textarea tabindex="4" id="message" placeholder="Type your message here..."  name="message"></textarea>
                   </fieldset>
 
-                  <fieldset>
                     <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
-                  </fieldset>
 
                   <div class="success"><?= $success ?></div>
                 </div>
@@ -137,7 +139,7 @@
       
   <!--contact-map -->
   <div class="address_mail_footer_grids">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3962.977217810282!2d3.321124050253201!3d6.649745423504272!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b914d72da5765%3A0x46bab0c9bd88ae1f!2sIju+Rd%2C+Lagos!5e0!3m2!1sen!2sng!4v1560336039060!5m2!1sen!2sng" width="100%" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3962.977217810282!2d3.321124050253201!3d6.649745423504272!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x103b914d72da5765%3A0x46bab0c9bd88ae1f!2sIju+Rd%2C+Lagos!5e0!3m2!1sen!2sng!4v1560336039060!5m2!1sen!2sng" width="100%" height="450"  style="border:0" allowfullscreen></iframe>
   </div>
   <!--//contact-map -->
       </main>
@@ -148,7 +150,7 @@
       <div class="row footer-logo">
           <div class="container">
               <div class="col-lg-5 col-md-5 col-sm-5 col-xs-5"></div>
-              <div class="col-lg-2 col-md-2 col-sm-4 col-xs-4"><img src="./img/CompanyLogo.png" alt="" logo></div>
+              <div class="col-lg-2 col-md-2 col-sm-4 col-xs-4"><img src="./img/CompanyLogo.png" alt="" ></div>
               <div class="col-lg-5 col-md-5 col-sm-4 col-xs-4"></div>
           </div>
         </div>
